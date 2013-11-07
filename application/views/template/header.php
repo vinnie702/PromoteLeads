6bbf114aca8936f283ad4c37904ec346c1c2328e
Brandon Vinall <brandonvinall@gmail.com>
2013-11-07 01:24:01 -0800

    starting new promoteleads public site

---
+++ application/views/template/header.php
@@ -0,0 +1,11 @@
+<?php  include_once 'headinclude.php'; ?>
+<div class='wrapper'>
+    <div class='row'>
+        <div class='headingLogos'>
+            <img src='/public/logo.png'>
+        </div>
+    </div>
+        <div class='mainContent'>
+<?php include_once 'nav.php'; ?>
+
+<?php // include_once 'alert.php' ?>
---
+++ application/views/template/headinclude.php
@@ -0,0 +1,14 @@
+<html lang="en">
+    <head>
+
+    <title>Promote Leads</title>
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <script type="text/javascript" src="/public/bootstrap3/js/bootstrap.min.js"></script>
+    <link type="text/css" href="/public/bootstrap3/css/bootstrap.min.css" rel="Stylesheet" />
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
