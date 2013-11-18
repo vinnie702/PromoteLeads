93902dbacfe13ea2c51f8773b288f3c1ef77341e
Brandon Vinall <brandonvinall@gmail.com>
2013-11-17 20:35:11 -0800

    tweaking css and videos

--- application/views/members/profile.php
+++ application/views/members/profile.php
@@ -15,7 +15,9 @@
     echo "<strong><p>{$user->addressLine1}</p>";
     echo "<p>{$user->addressLine2}</p>";
     echo "<p>{$user->addressLine3}</p>";
-    echo "<p>{$user->city}, {$user->state} {$user->postalCode}</p>";
+    echo "<p>";
+    if(!empty($user->city) OR (!empty($user->state))) echo "{$user->city}, {$user->state} ";
+    echo "{$user->postalCode}</p>";
     echo "</strong>";
     if(!empty($user->phone)) echo "<p>Office: {$user->phone}</p>";
     if(!empty($user->mobile)) echo "<p>Mobile: {$user->mobile}</p>";
@@ -50,19 +52,19 @@
     <div class='col-md-12'>
 
 <div class='tabbable'>
-<ul class="nav nav-tabs">
-  <li><a href="#Bio" class='purple' data-toggle="tab">Bio</a></li>
-  <li><a href="#contactMember" class='purple' data-toggle="tab">Contact Me!</a></li>
-  <li><a href="#Videos" class='purple' data-toggle="tab">Videos</a></li>
-</ul>
+    <ul class="nav nav-tabs">
+      <li><a href="#Bio" class='purple' data-toggle="tab">Bio</a></li>
+      <li><a href="#contactMember" class='purple' data-toggle="tab">Contact Me!</a></li>
+      <li><a href="#Videos" class='purple' data-toggle="tab">Videos</a></li>
+    </ul>
 
 <!-- Tab panes -->
 <div class="tab-content">
   <div class="tab-pane active OwnerBio purple-text" name='Bio' id="Bio">
 <?php
-        echo "<p>";
-            echo nl2br($user->bio);
-        echo "</p>";
+    echo "<p>";
+        echo nl2br($user->bio);
+    echo "</p>";
 ?>
   </div>
   <div class="tab-pane purple-text" id="contactMember" name='contactMember'>
@@ -75,34 +77,32 @@
         );
 
 echo form_open('welcome/saveContactForm', $attr);
-
-
 ?>
-        <div class="row">
-          <div class="form-group col-lg-4">
-            <label for="input1">Name</label>
-            <input type="text" name="name" class="form-control" id="name">
-          </div>
-          <div class="form-group col-lg-4">
-            <label for="input2">Email Address</label>
-            <input type="text" name="email" class="form-control" id="email">
-          </div>
-          <div class="form-group col-lg-4">
-            <label for="input3">Phone Number</label>
-            <input type="phone" name="phone" class="form-control" id="phone">
-          </div>
-          <div class="clearfix"></div>
-          <div class="form-group col-lg-12">
-            <label for="input4">Message</label>
-            <textarea name="message" class="form-control" rows="6" id="message"></textarea>
-          </div>
-          <div class="form-group col-lg-12">
-          <input type="hidden" name="userid" value="<?=$user->id?>">
-          <input type="hidden" name="page" value="/members/profile/<?=$user->id?>">
-            <button type="submit" id='submitBtn' name='submitBtn' class="btn btn-primary">Submit</button>
-          </div>
-     </div>
-        </form>
+    <div class="row">
+      <div class="form-group col-lg-4">
+        <label for="input1">Name</label>
+        <input type="text" name="name" class="form-control" id="name">
+      </div>
+      <div class="form-group col-lg-4">
+        <label for="input2">Email Address</label>
+        <input type="text" name="email" class="form-control" id="email">
+      </div>
+      <div class="form-group col-lg-4">
+        <label for="input3">Phone Number</label>
+        <input type="phone" name="phone" class="form-control" id="phone">
+      </div>
+      <div class="clearfix"></div>
+      <div class="form-group col-lg-12">
+        <label for="input4">Message</label>
+        <textarea name="message" class="form-control" rows="6" id="message"></textarea>
+      </div>
+      <div class="form-group col-lg-12">
+      <input type="hidden" name="userid" value="<?=$user->id?>">
+      <input type="hidden" name="page" value="/members/profile/<?=$user->id?>">
+        <button type="submit" id='submitBtn' name='submitBtn' class="btn btn-primary">Submit</button>
+      </div>
+ </div>
+    </form>
 
     </div> <!-- /.contactMember -->
   <div class="tab-pane Videos purple-text" id="Videos" name='Videos'>
@@ -116,7 +116,9 @@ echo form_open('welcome/saveContactForm', $attr);
             echo "<hr>";
             echo "<div class='row'>";
         }
+
         $ytid = $this->member->getYoutubeVideoID($r->url);
+
         echo <<< EOS
             <div class='ytContainer'>
                 <div class='yt-body'>
@@ -135,8 +137,8 @@ EOS;
         }
     }
 ?>
-</div>
-</div> <!-- /.tab-content -->
-</div> <!-- /.tabbable -->
+    </div>
+        </div> <!-- /.tab-content -->
+    </div> <!-- /.tabbable -->
     </div> <!-- /.col12 -->
 </div> <!-- /.row -->
